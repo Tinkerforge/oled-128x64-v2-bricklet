9cfee1041fd13f385ae66c033d0ec1ab0a273745
Erik Fleckstein <erik@tinkerforge.com>
2020-01-14 13:31:57 +0100

    examples: Use python3 input by default.

--- software/examples/python/example_load_image.py
+++ software/examples/python/example_load_image.py
@@ -38,5 +38,5 @@ if __name__ == "__main__":
 
     oled.write_pixels(0, 0, WIDTH-1, HEIGHT-1, pixels)
 
-    raw_input('Press key to exit\n') # Use input() in Python 3
+    input("Press key to exit\n") # Use raw_input() in Python 2
     ipcon.disconnect()
