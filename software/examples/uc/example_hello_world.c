5fb04eb860af1f8eb9c3a98ce2ea6469885bcb48
Erik Fleckstein <erik@tinkerforge.com>
2020-08-27 15:32:17 +0200

    uc: Fix warnings.

--- software/examples/uc/example_hello_world.c
+++ software/examples/uc/example_hello_world.c
@@ -5,7 +5,11 @@
 
 void check(int rc, const char* msg);
 
-TF_OLED128x64V2 oled;
+void example_setup(TF_HalContext *hal);
+void example_loop(TF_HalContext *hal);
+
+
+static TF_OLED128x64V2 oled;
 
 void example_setup(TF_HalContext *hal) {
 	// Create device object
