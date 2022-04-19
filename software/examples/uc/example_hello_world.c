e0ca44fa4aba4c718c1a56a22d53ea16205fe9be
Erik Fleckstein <erik@tinkerforge.com>
2022-04-19 16:04:01 +0200

    examples: uc: Rename TF_HalContext to TF_HAL.

--- software/examples/uc/example_hello_world.c
+++ software/examples/uc/example_hello_world.c
@@ -9,13 +9,13 @@
 
 void check(int rc, const char* msg);
 
-void example_setup(TF_HalContext *hal);
-void example_loop(TF_HalContext *hal);
+void example_setup(TF_HAL *hal);
+void example_loop(TF_HAL *hal);
 
 
 static TF_OLED128x64V2 oled;
 
-void example_setup(TF_HalContext *hal) {
+void example_setup(TF_HAL *hal) {
 	// Create device object
 	check(tf_oled_128x64_v2_create(&oled, UID, hal), "create device object");
 
@@ -26,7 +26,7 @@ void example_setup(TF_HalContext *hal) {
 	check(tf_oled_128x64_v2_write_line(&oled, 0, 0, "Hello World"), "call write_line");
 }
 
-void example_loop(TF_HalContext *hal) {
+void example_loop(TF_HAL *hal) {
 	// Poll for callbacks
 	tf_hal_callback_tick(hal, 0);
 }
